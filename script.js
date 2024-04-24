c38bf0e5829c3f5c03445f844ccbb265a11c3534
dwipawirabakti <dwipawirabakti@gmail.comgit config --global user.name dwipagit help>
2024-04-23 18:28:58 -0700

    memperbaiki file java script dan css

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 function convertTemperature() {
   const inputTemperature = parseFloat(document.getElementById("num").value);
+
   const unit = document.getElementById("unit").value;
+  console.log(unit);
 
   let convertedTemperature;
 
-  if (unit == "celcius") {
+  if (unit == "Celcius") {
     // Convert to fahrenheit
 
     convertedTemperature = (inputTemperature * 9) / 5 + 32;
-    Document.getElementById("result").textContent =
-      "${inputTemperature} degrees Celcius = ${convertedTemperature} degrees fahrenheit.";
-  }
-  else if(unit === 'Fahrenheit') {
+    console.log(convertedTemperature);
+    document.getElementById(
+      "result"
+    ).textContent = `${inputTemperature} degrees Celcius = ${convertedTemperature} degrees fahrenheit.`;
+  } else if (unit === "Fahrenheit") {
     // Convert to Celcius
-    convertedTemperature = (inputTemperature - 32 ) * 5/9;
-    document.getElementById("result").textContent = "${inputTemperature} degreess  fahrenheit = ${convertedTemperature} degrees Celcius.";
+    convertedTemperature = ((inputTemperature - 32) * 5) / 9;
+    document.getElementById(
+      "result"
+    ).textContent = `${inputTemperature} degreess  fahrenheit = ${convertedTemperature} degrees Celcius.`;
   } else {
-    document.getElementById("result").textContent = 'Please Select a valid unit.';
-  }
+    document.getElementById("result").textContent =
+      "Please Select a valid unit.";
   }
 }
